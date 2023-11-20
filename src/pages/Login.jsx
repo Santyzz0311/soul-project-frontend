cb44f76b5509368a13dfec4f86c55cbc5210ca28
Santyzz0311 <santyg0311@gmail.com>
2023-11-20 09:51:13 -0500

    XDDD

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -5,12 +5,12 @@ export default function Login() {
         <form className="flex flex-col gap-y-6 h-full justify-around">
           <h1 className="text-center text-2xl font-bold">Iniciar sesión</h1>
           <div className="grid grid-cols-[1fr_2fr]">
-            <label className="font-bold" htmlFor="email">Correo:</label>
-            <input className="w-full" type="email" name="email" id="email" />
+            <label className="font-bold place-self-center" htmlFor="email">Correo:</label>
+            <input className="w-full focus:outline-none p-2 rounded-md text-black" type="email" name="email" id="email" />
           </div>
           <div className="grid grid-cols-[1fr_2fr]">
-            <label className="font-bold" htmlFor="password">Contraseña:</label>
-            <input className="w-full" type="password" name="password" id="password" />
+            <label className="font-bold place-self-center" htmlFor="password">Contraseña:</label>
+            <input className="w-full focus:outline-none p-2 rounded-md text-black" type="password" name="password" id="password" />
           </div>
           <div className="flex justify-center items-center">
             <button className="bg-secondary hover:bg-tertiary text-white font-bold py-2 px-4 rounded">
