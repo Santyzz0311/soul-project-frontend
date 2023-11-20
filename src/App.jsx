d87cd6b3bee9cf0b59485f2c7eb66bd138bf5599
Santyzz0311 <santyg0311@gmail.com>
2023-11-20 09:08:28 -0500

    dsadsadsa

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
 import './App.css'
+import NavBar from './Components/NavBar'
 
 function App() {
   return (
     <>
+      <NavBar />
+      <main className='w-4/5  mx-auto'>
+
+      </main>
     </>
   )
 }
+
 export default App
