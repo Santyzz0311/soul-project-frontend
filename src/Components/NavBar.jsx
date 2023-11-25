ac80aa3d6f50ddb941e16c80dcdd10bb06261b33
Santyzz0311 <santyg0311@gmail.com>
2023-11-24 23:05:04 -0500

    :pig:

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -71,6 +71,11 @@ export default function NavBar() {
                   Productos
                 </Link>
               </li>
+              <li>
+                <Link className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0" to="/sellProduct">
+                  Vender Producto
+                </Link>
+              </li>
             </ul>
           </div>
         </div>
--- src/pages/SellProduct.jsx
+++ src/pages/SellProduct.jsx
@@ -1,60 +1,131 @@
+import { IoPersonSharp } from "react-icons/io5";
+import { MdPriceChange } from "react-icons/md";
+import { CgShutterstock } from "react-icons/cg";
+import { BiSolidCategory } from "react-icons/bi";
+import { FaX } from "react-icons/fa6";
+import { useState } from "react";
+import axios from "axios";
+
+
 export default function SellProduct() {
 
-  //   POST -> https://soul-server.vercel.app/Product/createProduct
-  // REQUEST BODY --> { nombre, precio, cantidad_stock, categoria, descripcion }
+  const [formData, setFormData] = useState({
+    nombre: "",
+    precio: "",
+    cantidad_stock: "",
+    categoria: "",
+    descripcion: ""
+  })
+
+  const [modalStates, setModalStates] = useState({
+    state: false,
+    description: ""
+  })
+
+  const handleChange = (e, key) => {
+    setFormData(prevState => ({ ...prevState, [key]: e.target.value }))
+  }
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+
+    if (formData.nombre === "") return
+
+    if (formData.precio === "") return
+
+    if (formData.cantidad_stock === "") return
+
+    if (formData.categoria === "") return
+
+    if (formData.descripcion === "") return
+
+    try {
+      const newValues = {
+        ...formData,
+        cantidad_stock: parseInt(formData.cantidad_stock),
+        precio: parseInt(formData.precio)
+      }
+
+      const response = await axios.post("https://soul-server.vercel.app/Product/createProduct", newValues)
+
+      setModalStates({
+        state: true,
+        description: response.data
+      })
+    } catch (e) {
+      console.log("D:", e);
+    } finally {
+      setFormData({
+        nombre: "",
+        precio: "",
+        cantidad_stock: "",
+        categoria: "",
+        descripcion: ""
+      })
+    }
+  }
+
   return (
     <div className="w-11/12 mx-auto my-8">
       <div className="w-full flex flex-col md:flex-row px-8 md:mx-0">
         <div className="w-full">
-          <h2 className="text-left text-4xl uppercase font-adventPro py-6">Contáctenos</h2>
-          <form>
+          <h2 className="text-center text-4xl uppercase font-adventPro py-6">Venda su Producto</h2>
+          <form onSubmit={handleSubmit}>
             <div className="grid grid-cols-1 grid-rows-1 gap-4">
               <div className="grid grid-cols-1 md:grid-cols-2 gap-4 w-full">
                 <div>
                   <div className="w-full relative">
-                    <label htmlFor="name" className="absolute top-1/2 -translate-y-1/2 flex border-r-[1px] border-[#979797] px-3"></label>
+                    <label htmlFor="name" className="absolute top-1/2 -translate-y-1/2 flex border-r-[1px] border-[#979797] px-3"><IoPersonSharp /></label>
                     <input
                       className={`outline-none bg-[#f4f4f4] text-black rounded-md w-full p-3 pl-16 placeholder:text-black border-[3px] `}
                       type="text"
-                      name="name"
-                      id="input-name"
-                      placeholder="Nombre"
+                      name="nombre"
+                      id="nombre"
+                      placeholder="Nombre del producto"
+                      value={formData.nombre}
+                      onChange={(e) => handleChange(e, "nombre")}
                     />
                   </div>
                 </div>
                 <div>
                   <div className="w-full relative">
-                    <label htmlFor="address" className="absolute top-1/2 -translate-y-1/2 flex border-r-[1px] border-[#979797] px-3"></label>
-                    <input
-                      className={`outline-none bg-[#f4f4f4] text-black rounded-md w-full p-3 pl-16 placeholder:text-black border-[3px] `}
-                      type="text"
-                      name="address"
-                      id="input-address"
-                      placeholder="Ubicación"
-                    />
-                  </div>
-                </div>
-                <div>
-                  <div className="w-full relative">
-                    <label htmlFor="email" className="absolute top-1/2 -translate-y-1/2 flex border-r-[1px] border-[#979797] px-3"></label>
+                    <label htmlFor="address" className="absolute top-1/2 -translate-y-1/2 flex border-r-[1px] border-[#979797] px-3"><MdPriceChange /></label>
                     <input
                       className={`outline-none bg-[#f4f4f4] text-black rounded-md w-full p-3 pl-16 placeholder:text-black border-[3px]`}
-                      type="email"
-                      name="email"
-                      id="input-address"
-                      placeholder="Correo Electrónico"
+                      type="text"
+                      name="precio"
+                      id="precio"
+                      placeholder="Precio"
+                      value={formData.precio}
+                      onChange={(e) => handleChange(e, "precio")}
                     />
                   </div>
                 </div>
                 <div>
                   <div className="w-full relative">
-                    <label htmlFor="number" className="absolute top-1/2 -translate-y-1/2 flex border-r-[1px] border-[#979797] px-3"></label>
+                    <label htmlFor="email" className="absolute top-1/2 -translate-y-1/2 flex border-r-[1px] border-[#979797] px-3"><CgShutterstock /></label>
+                    <input
+                      className={`outline-none bg-[#f4f4f4] text-black rounded-md w-full p-3 pl-16 placeholder:text-black border-[3px]`}
+                      type="text"
+                      name="cantidad_stock"
+                      id="cantidad_stock"
+                      placeholder="Ingrese el stock que tiene de este producto"
+                      value={formData.cantidad_stock}
+                      onChange={(e) => handleChange(e, "cantidad_stock")}
+                    />
+                  </div>
+                </div>
+                <div>
+                  <div className="w-full relative">
+                    <label htmlFor="number" className="absolute top-1/2 -translate-y-1/2 flex border-r-[1px] border-[#979797] px-3"><BiSolidCategory /></label>
                     <input
                       className={`outline-none bg-[#f4f4f4] text-black rounded-md w-full p-3 pl-16 placeholder:text-black border-[3px] `}
                       type="text"
-                      name="number"
-                      id="input-number"
-                      placeholder="Teléfono"
+                      name="categoria"
+                      id="categoria"
+                      placeholder="Categoría"
+                      value={formData.categoria}
+                      onChange={(e) => handleChange(e, "categoria")}
                     />
                   </div>
                 </div>
@@ -62,26 +133,14 @@ export default function SellProduct() {
               <div>
                 <textarea
                   className={`outline-none bg-[#f4f4f4] text-black resize-y rounded-md w-full placeholder:text-black p-4 border-[3px]`}
-                  placeholder="Cuéntanos, ¿cómo podemos ayudarte?"
-                  name="textarea"
-                  id="textarea-form"
+                  placeholder="Ingresa una descrición breve de tu producto"
+                  name="descripcion"
+                  id="descripcion"
+                  value={formData.descripcion}
+                  onChange={(e) => handleChange(e, "descripcion")}
                 ></textarea>
               </div>
-              <div>
-                <div className={`flex gap-2 rounded border-[3px]`}>
-                  <input
-                    type="checkbox"
-                    name="terms"
-                    id="input-checkbox"
-                    className='cursor-pointer w-5 h-5 rounded-md mt-1 ml-2'
-                  />
-                  <label htmlFor="input-checkbox" className="text-left cursor-pointer">
-                    Acepto el tratamiento que  le dará a mis datos
-                    personales, de acuerdo con la política de privacidad, las
-                    cuales he leído previamente.
-                  </label>
-                </div>
-              </div>
+
               <div className="mx-auto flex">
                 <button type="submit" className="bg-[#f4f4f4] rounded-md font-medium text-xl text-black py-1 px-16 hover:bg-grey-800">
                   Enviar
@@ -91,6 +150,16 @@ export default function SellProduct() {
           </form>
         </div>
       </div>
-    </div>
+      {
+        modalStates.state && (
+          <div className="absolute inset-0 h-full w-full backdrop-blur-lg flex items-center justify-center gap-x-4 font-extrabold">
+            Elemento registrado con éxito
+            <button onClick={() => setModalStates({ state: false, description: "" })}>
+              <FaX />
+            </button>
+          </div>
+        )
+      }
+    </div >
   )
 }
